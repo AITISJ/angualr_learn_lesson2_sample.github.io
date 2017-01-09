1877130db466fed162d5ea8e24e0111d6a1f77cd
AITISJ <zq_wx_gz_hao@qq.com>
2017-01-09 15:33:48 +0800

    init

---
+++ listen1_HelloWord/app.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+  var pathName = url.parse(req.url).pathname;
+
+  if (pathName == "/") {
+    pathName = "/index.html";
+  }
+  var filePath = __dirname + pathName;
+
+  res.statusCode = 200;
+  switch (path.extname(pathName)) {
+    case ".html":
+      res.setHeader("Content-Type", "text/html");
+      break;
+    case ".js":
+      res.setHeader("Content-Type", "text/javascript");
+      break;
+    case ".css":
+      res.setHeader("Content-Type", "text/css");
+      break;
+    case ".jpg":
+      res.setHeader("Content-Type", "image/jpeg");
+      break;
+  }
+  fs.readFile(filePath, function (err, data) {
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
