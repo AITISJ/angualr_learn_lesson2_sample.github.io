32776af56e2b8ce5e63e159f8074498a7c83fe2f
AITISJ <zq_wx_gz_hao@qq.com>
2017-02-23 15:30:42 +0800

    lesson2示例上传

---
+++ lesson2_Controller/controllers/index.js
@@ -0,0 +1,55 @@
+index.$inject = ['$scope'];
+function index(scope) {
+    var init = function () {
+        scope.step = 0;
+        scope.error = 0;
+        scope.accuracy = 0;
+        scope.questions = questions.sort(function (q1, q2) {
+            return Math.random() > 0.5 ? -1 : 1;
+        });
+        nextQuestion();
+    }
+
+    var nextQuestion = function () {
+        var question = scope.questions.shift();
+        scope.current = {
+            question: question.question,
+            options: question.options.sort(function (o1, o2) {
+                return Math.random() > 0.5 ? -1 : 1;
+            }),
+            answer: question.answer
+        }
+    }
+
+    var searchAnswer = function (option) {
+        if (option == scope.current.answer) {
+            scope.accuracy++;
+        }
+        else {
+            scope.error++;
+        }
+        if (scope.questions.length) {
+            nextQuestion();
+        }
+        else {
+            end();
+        }
+    }
+
+    var starting = function () {
+        scope.step = 1;
+    }
+
+    var end = function () {
+        scope.scores = 100 - scope.error*10;
+        scope.step = 2;
+    }
+
+    scope.starting = starting;
+    scope.end=end;
+    scope.searchAnswer=searchAnswer;
+    scope.nextQuestion=nextQuestion;
+    init();
+}
+
+app.controller('index', index);
---
+++ lesson2_Controller/questions.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+        "question": "苹果",
+        "options": [
+            "banana",
+            "apple",
+            "pear",
+            "keyboard"
+        ],
+        "answer": "apple"
+    },
+    {
+        "question": "橙子",
+        "options": [
+            "orange",
+            "oaange",
+            "orang",
+            "orage"
+        ],
+        "answer": "orange"
+    },
+    {
+        "question": "mouse",
+        "options": ["鼠标", "番薯", "红薯", "属地"],
+        "answer": "鼠标"
+    },
+    {
+        "question": "电脑",
+        "options": ["computer", "bag", "desk", "map"],
+        "answer": "computer"
+    },
+    {
+        "question": "书",
+        "options": ["book", "bok", "block", "note"],
+        "answer": "book"
+    },
+    {
+        "question": "eraser",
+        "options": ["橡皮擦", "橡皮刀", "橡皮圈", "橡皮胶"],
+        "answer": "橡皮擦"
+    },
+    {
+        "question": "pen",
+        "options": ["钢笔", "铅笔", "圆珠笔", "毛笔"],
+        "answer": "钢笔"
+    },
+    {
+        "question": "钟",
+        "options": ["clock", "cloud", "code", "change"],
+        "answer": "clock"
+    },
+    {
+        "question": "数字",
+        "options": ["number", "name", "nike", "nice"],
+        "answer": "number"
+    },
+    {
+        "question": "班级",
+        "options": ["class", "glass", "hass", "css"],
+        "answer": "班级"
+    }
+];
